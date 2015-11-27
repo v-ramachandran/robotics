e4fc2c8ca0e8257e0ec0d04a4eaf383a2381f7b2
ankitade <ankitade@gmail.com>
2015-11-26 21:13:11 -0800

    line point detection for transition

--- core/vision/LineDetector.cpp
+++ core/vision/LineDetector.cpp
@@ -9,36 +9,36 @@ void LineDetector::findLinePointCandidates(ImageProcessor * processor){
   unsigned char * color_table_ = processor->getColorTable();
   int height = processor->getImageHeight();
   int width = processor->getImageWidth();
-	
+	int threshold =0;
   for(int y=0; y<height; ++y){
     if(y%5==0){
       for(int x=0; x<width; ++x){
+				LinePoint point;
+				ColorTableMethods::xy2yuv(image, x, y, width, point.y, point.u, point.v);
+				point.PosX = x;
+				point.PosY = y;
         Color currentColor = ColorTableMethods::xy2color(image, color_table_, x, y, width);
-				if(currentColor==c_WHITE){
-					LinePoint point;
-					ColorTableMethods::xy2yuv(image, x, y, width, point.y, point.u, point.v);
-					point.PosX = x;
-					point.PosY = y;
+				if(y>threshold){
+				
 					linePoints.push_back(point);
 				}					 
       }
     }
     else{
       for(int x=0; x<width; x=x+5){
+				LinePoint point;
+				ColorTableMethods::xy2yuv(image, x, y, width, point.y, point.u, point.v);
+				point.PosX = x;
+				point.PosY = y;
         Color currentColor = ColorTableMethods::xy2color(image, color_table_, x, y, processor->getImageWidth());
-				if(currentColor==c_WHITE){
-					if(currentColor==c_WHITE){
-						LinePoint point;
-						ColorTableMethods::xy2yuv(image, x, y, width, point.y, point.u, point.v);
-						point.PosX = x;
-						point.PosY = y;
+				if(y>threshold){
+						
 						linePoints.push_back(point);
-				}					  
-      }
-    }
-  }
+				}
+    	}
+  	}
 
   
-}
+	}
 }
 
